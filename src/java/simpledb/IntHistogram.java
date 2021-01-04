c181533af3ea04f3208eaec136b12b0bb394f61a
Patrick Nicholas <patricknicholas@foxmail.com>
2021-01-04 22:09:58 +0800

    Done lab3 exercise 1

--- src/java/simpledb/IntHistogram.java
+++ src/java/simpledb/IntHistogram.java
@@ -1,72 +1,110 @@
 package simpledb;
 
-/** A class to represent a fixed-width histogram over a single integer-based field.
+import java.util.stream.Stream;
+
+/**
+ * A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
+    private int ntups, min, max;
+    private double width;
+    private int[] buckets;
 
     /**
      * Create a new IntHistogram.
-     * 
+     * <p>
      * This IntHistogram should maintain a histogram of integer values that it receives.
      * It should split the histogram into "buckets" buckets.
-     * 
+     * <p>
      * The values that are being histogrammed will be provided one-at-a-time through the "addValue()" function.
-     * 
+     * <p>
      * Your implementation should use space and have execution time that are both
-     * constant with respect to the number of values being histogrammed.  For example, you shouldn't 
+     * constant with respect to the number of values being histogrammed.  For example, you shouldn't
      * simply store every value that you see in a sorted list.
-     * 
+     *
      * @param buckets The number of buckets to split the input value into.
-     * @param min The minimum integer value that will ever be passed to this class for histogramming
-     * @param max The maximum integer value that will ever be passed to this class for histogramming
+     * @param min     The minimum integer value that will ever be passed to this class for histogramming
+     * @param max     The maximum integer value that will ever be passed to this class for histogramming
      */
     public IntHistogram(int buckets, int min, int max) {
-    	// some code goes here
+        this.buckets = new int[buckets];
+        this.min = min;
+        this.max = max;
+        this.ntups = 0;
+        this.width = (max - min + 1.) / buckets;
+    }
+
+    private int getIndex(int v) {
+        return (int) ((v - min) / width);
     }
 
     /**
      * Add a value to the set of values that you are keeping a histogram of.
+     *
      * @param v Value to add to the histogram
      */
     public void addValue(int v) {
-    	// some code goes here
+        if (this.min <= v && v <= this.max) {
+            this.buckets[getIndex(v)]++;
+            this.ntups++;
+        }
     }
 
     /**
      * Estimate the selectivity of a particular predicate and operand on this table.
-     * 
-     * For example, if "op" is "GREATER_THAN" and "v" is 5, 
+     * <p>
+     * For example, if "op" is "GREATER_THAN" and "v" is 5,
      * return your estimate of the fraction of elements that are greater than 5.
-     * 
+     *
      * @param op Operator
-     * @param v Value
+     * @param v  Value
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
+        switch (op) {
+            case LESS_THAN: {
+                if (v <= min) return 0.0;
+                if (v >= max) return 1.0;
+                final int index = getIndex(v);
+                double cnt = 0;
+                for (int i = 0; i < index; ++i) {
+                    cnt += buckets[i];
+                }
+                cnt += buckets[index] / width * (v - index * width - min);
+                return cnt / ntups;
+            }
+            case LESS_THAN_OR_EQ:
+                return estimateSelectivity(Predicate.Op.LESS_THAN, v + 1);
+            case GREATER_THAN:
+                return 1 - estimateSelectivity(Predicate.Op.LESS_THAN_OR_EQ, v);
+            case GREATER_THAN_OR_EQ:
+                return estimateSelectivity(Predicate.Op.GREATER_THAN, v - 1);
+            case EQUALS:
+                return estimateSelectivity(Predicate.Op.LESS_THAN_OR_EQ, v) -
+                        estimateSelectivity(Predicate.Op.LESS_THAN, v);
+            case NOT_EQUALS:
+                return 1 - estimateSelectivity(Predicate.Op.EQUALS, v);
 
-    	// some code goes here
-        return -1.0;
+        }
+        return 0.0;
     }
-    
+
     /**
-     * @return
-     *     the average selectivity of this histogram.
-     *     
-     *     This is not an indispensable method to implement the basic
-     *     join optimization. It may be needed if you want to
-     *     implement a more efficient optimization
-     * */
-    public double avgSelectivity()
-    {
-        // some code goes here
+     * @return the average selectivity of this histogram.
+     * <p>
+     * This is not an indispensable method to implement the basic
+     * join optimization. It may be needed if you want to
+     * implement a more efficient optimization
+     */
+    public double avgSelectivity() {
         return 1.0;
     }
-    
+
     /**
      * @return A string describing this histogram, for debugging purposes
      */
     public String toString() {
-        // some code goes here
-        return null;
+        return String.format("IntHistogram(buckets=%d, min=%d, max=%d)",
+                buckets.length, min, max);
     }
 }
